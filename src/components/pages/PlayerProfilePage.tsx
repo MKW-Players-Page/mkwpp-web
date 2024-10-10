614d211e833b194c9e3364c4f39cffdbf8293ef9
FallBackITA27 <diegomassanova4@gmail.com>
2024-10-10 18:46:38 +0200

    CategorySelect and LapModeSelect style

--- src/components/pages/PlayerProfilePage.tsx
+++ src/components/pages/PlayerProfilePage.tsx
@@ -8,177 +8,179 @@ import api, { CategoryEnum } from '../../api';
 import { useApi } from '../../hooks/ApiHook';
 import { formatDate, formatTime } from '../../utils/Formatters';
 import {
-  getRegionById, getRegionNameFull, getStandardLevel, MetadataContext
+    getRegionById, getRegionNameFull, getStandardLevel, MetadataContext
 } from '../../utils/Metadata';
 import { integerOr } from '../../utils/Numbers';
 import { LapModeEnum } from '../widgets/LapModeSelect';
 
 const PlayerProfilePage = () => {
-  const { id: idStr } = useParams();
-  const id = Math.max(integerOr(idStr, 0), 0);
+    const { id: idStr } = useParams();
+    const id = Math.max(integerOr(idStr, 0), 0);
 
-  const [category, setCategory] = useState<CategoryEnum>(CategoryEnum.NonShortcut);
-  const [lapMode, setLapMode] = useState<LapModeEnum>(LapModeEnum.Overall);
+    const [category, setCategory] = useState<CategoryEnum>(CategoryEnum.NonShortcut);
+    const [lapMode, setLapMode] = useState<LapModeEnum>(LapModeEnum.Overall);
 
-  const metadata = useContext(MetadataContext);
+    const metadata = useContext(MetadataContext);
 
-  const {
-    isLoading: playerLoading,
-    data: player,
-    error: playerError,
-  } = useApi(() => api.timetrialsPlayersRetrieve({ id }));
+    const {
+        isLoading: playerLoading,
+        data: player,
+        error: playerError,
+    } = useApi(() => api.timetrialsPlayersRetrieve({ id }));
 
-  const {
-    isLoading: statsLoading,
-    data: stats,
-  } = useApi(() => api.timetrialsPlayersStatsRetrieve({
-    id,
-    category,
-    lapMode,
-    region: 1,
-  }), [category, lapMode]);
+    const {
+        isLoading: statsLoading,
+        data: stats,
+    } = useApi(() => api.timetrialsPlayersStatsRetrieve({
+        id,
+        category,
+        lapMode,
+        region: 1,
+    }), [category, lapMode]);
 
-  const {
-    isLoading: scoresLoading,
-    data: scores
-  } = useApi(() => api.timetrialsPlayersScoresList({ id, category }), [category]);
+    const {
+        isLoading: scoresLoading,
+        data: scores
+    } = useApi(() => api.timetrialsPlayersScoresList({ id, category }), [category]);
 
-  return (
-    <>
-      {/* Redirect to player list if id is invalid or does not exist. */}
-      {playerError && <Navigate to={resolvePage(Pages.PlayerList)} />}
-      <h1>
-        <FlagIcon region={getRegionById(metadata, player?.region || 0)} />
-        {player?.name || <>&nbsp;</>}
-      </h1>
-      <CategorySelect value={category} onChange={setCategory} />
-      <LapModeSelect includeOverall value={lapMode} onChange={setLapMode} />
-      <div className="module-row">
-        <div className="module">
-          <Deferred isWaiting={playerLoading}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Location</td>
-                  <td>{getRegionNameFull(metadata, player?.region || 0)}</td>
-                </tr>
-                <tr>
-                  <td>Alias</td>
-                  <td>{player?.alias}</td>
-                </tr>
-                <tr>
-                  <td>Date Joined</td>
-                  <td>{player?.joinedDate && formatDate(player.joinedDate)}</td>
-                </tr>
-                <tr>
-                  <td>Last Activity</td>
-                  <td>{player?.lastActivity && formatDate(player.lastActivity)}</td>
-                </tr>
-              </tbody>
-            </table>
-          </Deferred>
-        </div>
-        <div className="module">
-          <Deferred isWaiting={statsLoading}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Average Finish</td>
-                  <td>
-                    {stats && stats.scoreCount > 0 ? stats.totalRank / stats.scoreCount : "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <td>ARR</td>
-                  <td>
-                    {stats && stats.scoreCount > 0 ? stats.totalStandard / stats.scoreCount : "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <td>PR:WR</td>
-                  <td>
-                    {stats && stats.scoreCount > 0 ? (
-                      (stats.totalRecordRatio / stats.scoreCount * 100).toFixed(4) + "%"
-                    ) : "-"}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Total Time</td>
-                  <td>{stats ? formatTime(stats.totalScore) : "-"}</td>
-                </tr>
-              </tbody>
-            </table>
-          </Deferred>
-        </div>
-        <div className="module">
-          <Deferred isWaiting={playerLoading}>
-            {/* Temporary until better solution implemented, like a popup dialog. */}
-            <div className="module-content" style={{overflowY: 'scroll', maxHeight: 128}}>
-              <p>
-                {player?.bio ? player.bio.split('\n').map((line: string) => (
-                  <>{line}<br /></>
-                )) : (
-                  <i>This player doesn't have anything to say about themselves...</i>
-                )}
-              </p>
+    return (
+        <>
+            {/* Redirect to player list if id is invalid or does not exist. */}
+            {playerError && <Navigate to={resolvePage(Pages.PlayerList)} />}
+            <h1>
+                <FlagIcon region={getRegionById(metadata, player?.region || 0)} />
+                {player?.name || <>&nbsp;</>}
+            </h1>
+            <div className="module-row">
+                <CategorySelect value={category} onChange={setCategory} />
+                <LapModeSelect includeOverall value={lapMode} onChange={setLapMode} />
             </div>
-          </Deferred>
-        </div>
-      </div>
-      <div className="module">
-        <Deferred isWaiting={metadata.isLoading || scoresLoading}>
-          <table>
-            <thead>
-              <tr>
-                <th>Track</th>
-                <th>Course</th>
-                <th>Lap</th>
-                <th>Rank</th>
-                <th>Standard</th>
-                <th>PR:WR</th>
-                <th>Date</th>
-                <th className="col-icon" />
-                <th className="col-icon" />
-              </tr>
-            </thead>
-            <tbody>
-              {metadata.tracks?.map((track) => [false, true].map((isLap) => {
-                const score = scores?.find(
-                  (score) => score.track === track.id && score.isLap === isLap
-                );
-                return (
-                  <tr key={`${isLap ? 'l' : 'c'}${track.id}`}>
-                    {!isLap && (
-                      <td rowSpan={2}>
-                        <Link to={resolvePage(Pages.TrackChart, { id: track.id })}>
-                          {track.name}
-                        </Link>
-                      </td>
-                    )}
-                    {isLap && <td />}
-                    <td className={score?.category !== category ? 'fallthrough' : ''}>
-                      {score ? formatTime(score.value) : "-"}
-                    </td>
-                    {!isLap && <td />}
-                    <td>{score?.rank || "-"}</td>
-                    <td>{score ? getStandardLevel(metadata, score.standard)?.name : "-"}</td>
-                    <td>{score ? (score.recordRatio * 100).toFixed(2) + "%" : "-"}</td>
-                    <td>{score?.date ? formatDate(score.date) : "-"}</td>
-                    <td>{score?.videoLink && (
-                      <a href={score.videoLink} target="_blank" rel="noopener noreferrer">V</a>
-                    )}</td>
-                    <td>{score?.ghostLink && (
-                      <a href={score.ghostLink} target="_blank" rel="noopener noreferrer">G</a>
-                    )}</td>
-                  </tr>
-                );
-              }))}
-            </tbody>
-          </table>
-        </Deferred>
-      </div>
-    </>
-  );
+            <div className="module-row">
+                <div className="module">
+                    <Deferred isWaiting={playerLoading}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Location</td>
+                                    <td>{getRegionNameFull(metadata, player?.region || 0)}</td>
+                                </tr>
+                                <tr>
+                                    <td>Alias</td>
+                                    <td>{player?.alias}</td>
+                                </tr>
+                                <tr>
+                                    <td>Date Joined</td>
+                                    <td>{player?.joinedDate && formatDate(player.joinedDate)}</td>
+                                </tr>
+                                <tr>
+                                    <td>Last Activity</td>
+                                    <td>{player?.lastActivity && formatDate(player.lastActivity)}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Deferred>
+                </div>
+                <div className="module">
+                    <Deferred isWaiting={statsLoading}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Average Finish</td>
+                                    <td>
+                                        {stats && stats.scoreCount > 0 ? stats.totalRank / stats.scoreCount : "-"}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>ARR</td>
+                                    <td>
+                                        {stats && stats.scoreCount > 0 ? stats.totalStandard / stats.scoreCount : "-"}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>PR:WR</td>
+                                    <td>
+                                        {stats && stats.scoreCount > 0 ? (
+                                            (stats.totalRecordRatio / stats.scoreCount * 100).toFixed(4) + "%"
+                                        ) : "-"}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Total Time</td>
+                                    <td>{stats ? formatTime(stats.totalScore) : "-"}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Deferred>
+                </div>
+                <div className="module">
+                    <Deferred isWaiting={playerLoading}>
+                        {/* Temporary until better solution implemented, like a popup dialog. */}
+                        <div className="module-content" style={{ overflowY: 'scroll', maxHeight: 128 }}>
+                            <p>
+                                {player?.bio ? player.bio.split('\n').map((line: string) => (
+                                    <>{line}<br /></>
+                                )) : (
+                                    <i>This player doesn't have anything to say about themselves...</i>
+                                )}
+                            </p>
+                        </div>
+                    </Deferred>
+                </div>
+            </div>
+            <div className="module">
+                <Deferred isWaiting={metadata.isLoading || scoresLoading}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Track</th>
+                                <th>Course</th>
+                                <th>Lap</th>
+                                <th>Rank</th>
+                                <th>Standard</th>
+                                <th>PR:WR</th>
+                                <th>Date</th>
+                                <th className="col-icon" />
+                                <th className="col-icon" />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {metadata.tracks?.map((track) => [false, true].map((isLap) => {
+                                const score = scores?.find(
+                                    (score) => score.track === track.id && score.isLap === isLap
+                                );
+                                return (
+                                    <tr key={`${isLap ? 'l' : 'c'}${track.id}`}>
+                                        {!isLap && (
+                                            <td rowSpan={2}>
+                                                <Link to={resolvePage(Pages.TrackChart, { id: track.id })}>
+                                                    {track.name}
+                                                </Link>
+                                            </td>
+                                        )}
+                                        {isLap && <td />}
+                                        <td className={score?.category !== category ? 'fallthrough' : ''}>
+                                            {score ? formatTime(score.value) : "-"}
+                                        </td>
+                                        {!isLap && <td />}
+                                        <td>{score?.rank || "-"}</td>
+                                        <td>{score ? getStandardLevel(metadata, score.standard)?.name : "-"}</td>
+                                        <td>{score ? (score.recordRatio * 100).toFixed(2) + "%" : "-"}</td>
+                                        <td>{score?.date ? formatDate(score.date) : "-"}</td>
+                                        <td>{score?.videoLink && (
+                                            <a href={score.videoLink} target="_blank" rel="noopener noreferrer">V</a>
+                                        )}</td>
+                                        <td>{score?.ghostLink && (
+                                            <a href={score.ghostLink} target="_blank" rel="noopener noreferrer">G</a>
+                                        )}</td>
+                                    </tr>
+                                );
+                            }))}
+                        </tbody>
+                    </table>
+                </Deferred>
+            </div>
+        </>
+    );
 };
 
 export default PlayerProfilePage;
--- src/components/pages/RankingsPage.tsx
+++ src/components/pages/RankingsPage.tsx
@@ -13,111 +13,113 @@ import { getRegionById, MetadataContext } from '../../utils/Metadata';
 import { UserContext } from '../../utils/User';
 
 export interface RankingsMetric {
-  title: string;
-  description: string;
-  metric: MetricEnum;
-  getValueString: (player: PlayerStats) => string;
+    title: string;
+    description: string;
+    metric: MetricEnum;
+    getValueString: (player: PlayerStats) => string;
 };
 
 export type RankingsMetricMap = {
-  [key: string]: RankingsMetric;
+    [key: string]: RankingsMetric;
 };
 
 export const RankingsMetrics: RankingsMetricMap = {
-  AverageFinish: {
-    title: "Average Finish",
-    description:
-      "Average Finish (AF for short) is the average of a player's ranking across all tracks.",
-    metric: 'total_rank',
-    getValueString: (stats) => String(stats.totalRank / stats.scoreCount),
-  },
-  AverageStandard: {
-    title: "ARR",
-    description:
-      "Average Rank Rating (ARR for short) is the average standard of a player's time across all " +
-      "tracks.",
-    metric: 'total_standard',
-    getValueString: (stats) => String(stats.totalStandard / stats.scoreCount),
-  },
-  AverageRecordRatio: {
-    title: "PR:WR",
-    description:
-      "Personal Record to World Record ratio (PR:WR) is calculated by dividing the world record " +
-      "time by the player's time. Players are ranked by the average of their PR:WR across all " +
-      "tracks.",
-    metric: 'total_record_ratio',
-    getValueString: (stats) => (
-      (stats.totalRecordRatio / stats.scoreCount * 100).toFixed(4) + "%"
-    ),
-  },
-  TotalTime: {
-    title: "Total Time",
-    description: "Total time is the sum of a player's fastest times across all tracks.",
-    metric: 'total_score',
-    getValueString: (stats) => formatTime(stats.totalScore),
-  },
+    AverageFinish: {
+        title: "Average Finish",
+        description:
+            "Average Finish (AF for short) is the average of a player's ranking across all tracks.",
+        metric: 'total_rank',
+        getValueString: (stats) => String(stats.totalRank / stats.scoreCount),
+    },
+    AverageStandard: {
+        title: "ARR",
+        description:
+            "Average Rank Rating (ARR for short) is the average standard of a player's time across all " +
+            "tracks.",
+        metric: 'total_standard',
+        getValueString: (stats) => String(stats.totalStandard / stats.scoreCount),
+    },
+    AverageRecordRatio: {
+        title: "PR:WR",
+        description:
+            "Personal Record to World Record ratio (PR:WR) is calculated by dividing the world record " +
+            "time by the player's time. Players are ranked by the average of their PR:WR across all " +
+            "tracks.",
+        metric: 'total_record_ratio',
+        getValueString: (stats) => (
+            (stats.totalRecordRatio / stats.scoreCount * 100).toFixed(4) + "%"
+        ),
+    },
+    TotalTime: {
+        title: "Total Time",
+        description: "Total time is the sum of a player's fastest times across all tracks.",
+        metric: 'total_score',
+        getValueString: (stats) => formatTime(stats.totalScore),
+    },
 };
 
 export interface RankingsProps {
-  metric: RankingsMetric;
+    metric: RankingsMetric;
 };
 
 const RankingsPage = ({ metric }: RankingsProps) => {
-  const [category, setCategory] = useState<CategoryEnum>(CategoryEnum.NonShortcut);
-  const [lapMode, setLapMode] = useState<LapModeEnum>(LapModeEnum.Overall);
+    const [category, setCategory] = useState<CategoryEnum>(CategoryEnum.NonShortcut);
+    const [lapMode, setLapMode] = useState<LapModeEnum>(LapModeEnum.Overall);
 
-  const metadata = useContext(MetadataContext);
+    const metadata = useContext(MetadataContext);
 
-  const { user } = useContext(UserContext);
+    const { user } = useContext(UserContext);
 
-  const {
-    isLoading,
-    data: rankings,
-  } = useApi(() => api.timetrialsRankingsList({
-    category,
-    lapMode,
-    region: 1,
-    metric: metric.metric,
-  }), [category, lapMode]);
+    const {
+        isLoading,
+        data: rankings,
+    } = useApi(() => api.timetrialsRankingsList({
+        category,
+        lapMode,
+        region: 1,
+        metric: metric.metric,
+    }), [category, lapMode]);
 
-  return (
-    <>
-      <h1>{metric.title}</h1>
-      <p>{metric.description}</p>
-      <CategorySelect value={category} onChange={setCategory} />
-      <LapModeSelect includeOverall value={lapMode} onChange={setLapMode} />
-      <div className="module">
-        <Deferred isWaiting={isLoading}>
-          <table>
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Player</th>
-                <th>{metric.title}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {rankings?.map((stats) => (
-                <tr
-                  key={stats.player.id}
-                  className={user && stats.player.id === user.player ? 'highlighted' : ''}
-                >
-                  <td>{stats.rank}</td>
-                  <td>
-                    <FlagIcon region={getRegionById(metadata, stats.player.region || 0)} />
-                    <Link to={resolvePage(Pages.PlayerProfile, { id: stats.player.id })}>
-                      {stats.player.alias || stats.player.name}
-                    </Link>
-                  </td>
-                  <td>{metric.getValueString(stats)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </Deferred>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1>{metric.title}</h1>
+            <p>{metric.description}</p>
+            <div className='module-row'>
+                <CategorySelect value={category} onChange={setCategory} />
+                <LapModeSelect includeOverall value={lapMode} onChange={setLapMode} />
+            </div>
+            <div className="module">
+                <Deferred isWaiting={isLoading}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Rank</th>
+                                <th>Player</th>
+                                <th>{metric.title}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rankings?.map((stats) => (
+                                <tr
+                                    key={stats.player.id}
+                                    className={user && stats.player.id === user.player ? 'highlighted' : ''}
+                                >
+                                    <td>{stats.rank}</td>
+                                    <td>
+                                        <FlagIcon region={getRegionById(metadata, stats.player.region || 0)} />
+                                        <Link to={resolvePage(Pages.PlayerProfile, { id: stats.player.id })}>
+                                            {stats.player.alias || stats.player.name}
+                                        </Link>
+                                    </td>
+                                    <td>{metric.getValueString(stats)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </Deferred>
+            </div>
+        </>
+    );
 };
 
 export default RankingsPage;
