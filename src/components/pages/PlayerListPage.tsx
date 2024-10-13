12a614b44f726c4b67e5f5c95f3a542425702ff6
Leila Massanova <49619073+FallBackITA27@users.noreply.github.com>
2024-10-13 22:52:10 +0200

    text input in player list (#14)
    * text input in player list
    
    * different approach

--- src/components/pages/PlayerListPage.tsx
+++ src/components/pages/PlayerListPage.tsx
@@ -8,17 +8,60 @@ import api from "../../api";
 import { useApi } from "../../hooks/ApiHook";
 import { getRegionById, getRegionNameFull, MetadataContext } from "../../utils/Metadata";
 import { UserContext } from "../../utils/User";
+import { PlayerBasic } from "../../api";
+import { useState } from "react";
+
+interface PlayerForFilter extends PlayerBasic {
+  simplifiedName: string;
+}
 
 const PlayerListPage = () => {
-  const { isLoading, data: players } = useApi(() => api.timetrialsPlayersList());
+  let { isLoading, data: players } = useApi(() => api.timetrialsPlayersList());
+  players?.forEach(
+    (r) => ((r as PlayerForFilter).simplifiedName = r.name.toLowerCase().normalize("NFKD")),
+  );
 
   const metadata = useContext(MetadataContext);
 
   const { user } = useContext(UserContext);
 
+  let [playerFilter, setPlayerFilter] = useState("");
+
   return (
     <>
       <h1>Players</h1>
+      <div
+        style={
+          {
+            display: "grid",
+            gridTemplateColumns: "4fr 1fr",
+            gridGap: "5px",
+          } as React.CSSProperties
+        }
+      >
+        <input
+          id="filterText"
+          type="text"
+          className="module"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") document.getElementById("searchBtn")?.click();
+          }}
+        />
+        <button
+          style={
+            {
+              borderRadius: 0,
+            } as React.CSSProperties
+          }
+          id="searchBtn"
+          className="module"
+          onClick={(e) => {
+            setPlayerFilter((document.getElementById("filterText") as HTMLInputElement).value);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="module">
         <Deferred isWaiting={isLoading}>
           <table>
@@ -29,20 +72,26 @@ const PlayerListPage = () => {
               </tr>
             </thead>
             <tbody className="table-hover-rows">
-              {players?.map((player) => (
-                <tr
-                  key={player.id}
-                  className={user && player.id === user.player ? "highlighted" : ""}
-                >
-                  <td>
-                    <FlagIcon region={getRegionById(metadata, player.region || 0)} />
-                    <Link to={resolvePage(Pages.PlayerProfile, { id: player.id })}>
-                      {player.name}
-                    </Link>
-                  </td>
-                  <td>{getRegionNameFull(metadata, player.region || 0)}</td>
-                </tr>
-              ))}
+              {players?.map((player) =>
+                (player as PlayerForFilter).simplifiedName.includes(
+                  playerFilter.toLowerCase().normalize("NFKD"),
+                ) ? (
+                  <tr
+                    key={player.id}
+                    className={user && player.id === user.player ? "highlighted" : ""}
+                  >
+                    <td>
+                      <FlagIcon region={getRegionById(metadata, player.region || 0)} />
+                      <Link to={resolvePage(Pages.PlayerProfile, { id: player.id })}>
+                        {player.name}
+                      </Link>
+                    </td>
+                    <td>{getRegionNameFull(metadata, player.region || 0)}</td>
+                  </tr>
+                ) : (
+                  <></>
+                ),
+              )}
             </tbody>
           </table>
         </Deferred>
