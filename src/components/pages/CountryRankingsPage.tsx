2d45caf0c9d47259b89d1a931bc33ca2af8a0d85
FallBackITA27 <FallBackITA27@users.noreply.github.com>
2024-10-29 17:39:55 +0000

    Automatic Linting Process

--- src/components/pages/CountryRankingsPage.tsx
+++ src/components/pages/CountryRankingsPage.tsx
@@ -7,29 +7,23 @@ import api from "../../api";
 import { useApi } from "../../hooks";
 import { getCategorySiteHue } from "../../utils/EnumUtils";
 import OverwriteColor from "../widgets/OverwriteColor";
-import {
-  useCategoryParam,
-  useLapModeParam,
-  useRowHighlightParam,
-} from "../../utils/SearchParams";
+import { useCategoryParam, useLapModeParam, useRowHighlightParam } from "../../utils/SearchParams";
 
-const CountryRankingsPage = (
-    
-) => {
+const CountryRankingsPage = () => {
   const searchParams = useSearchParams();
   const { category, setCategory } = useCategoryParam(searchParams, ["hl"]);
   const { lapMode, setLapMode } = useLapModeParam(searchParams, false, ["hl"]);
   const highlight = useRowHighlightParam(searchParams).highlight;
 
-    const top = 3;
-  
+  const top = 3;
+
   const { isLoading, data } = useApi(
     () =>
       api.timetrialsRegionsRankingsList({
         category,
         lapMode,
         top,
-        type: "continent"
+        type: "continent",
       }),
     [category, lapMode],
   );
@@ -49,7 +43,10 @@ const CountryRankingsPage = (
   return (
     <>
       <h1>Country Rankings</h1>
-      <p>The best {top} times for each country are taken for each track, and averaged. This value is then averaged over all tracks, like Average Finish.</p>
+      <p>
+        The best {top} times for each country are taken for each track, and averaged. This value is
+        then averaged over all tracks, like Average Finish.
+      </p>
       <OverwriteColor hue={siteHue}>
         <div className="module-row">
           <CategorySelect value={category} onChange={setCategory} />
@@ -66,48 +63,40 @@ const CountryRankingsPage = (
                 </tr>
               </thead>
               <tbody className="table-hover-rows">
-                              {data?.map((stats, idx, arr) => {
-                                  const calculatedValueStr = (stats.totalRank / stats.scoreCount).toFixed(4); const calculatedValue = parseFloat(calculatedValueStr); return (
-                  <>
-                    {highlight &&
-                    (calculatedValue > highlight &&
-                        (arr[idx - 1] === undefined ||
-                          (arr[idx - 1].totalRank / arr[idx - 1].scoreCount)< highlight)) ? (
-                      <>
-                        <tr ref={highlightElement} key={highlight} className="highlighted">
-                          <td />
-                          <td>Your Highlighted Value</td>
-                          <td>
-                            {highlight}
-                          </td>
-                        </tr>
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <tr
-                      key={stats.region.code}
-                      className={
-                        calculatedValue === highlight
-                          ? "highlighted"
-                          : ""
-                      }
-                      ref={
-                        calculatedValue === highlight ? highlightElement : undefined
-                      }
-                    >
-                      <td>{stats.rank}</td>
-                      <td>
-                        <FlagIcon region={stats.region} />
-                        <p
-                        >
-                          {stats.region.name}
-                        </p>
-                      </td>
-                      <td>{calculatedValueStr}</td>
-                    </tr>
-                  </>
-                )})}
+                {data?.map((stats, idx, arr) => {
+                  const calculatedValueStr = (stats.totalRank / stats.scoreCount).toFixed(4);
+                  const calculatedValue = parseFloat(calculatedValueStr);
+                  return (
+                    <>
+                      {highlight &&
+                      calculatedValue > highlight &&
+                      (arr[idx - 1] === undefined ||
+                        arr[idx - 1].totalRank / arr[idx - 1].scoreCount < highlight) ? (
+                        <>
+                          <tr ref={highlightElement} key={highlight} className="highlighted">
+                            <td />
+                            <td>Your Highlighted Value</td>
+                            <td>{highlight}</td>
+                          </tr>
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                      <tr
+                        key={stats.region.code}
+                        className={calculatedValue === highlight ? "highlighted" : ""}
+                        ref={calculatedValue === highlight ? highlightElement : undefined}
+                      >
+                        <td>{stats.rank}</td>
+                        <td>
+                          <FlagIcon region={stats.region} />
+                          <p>{stats.region.name}</p>
+                        </td>
+                        <td>{calculatedValueStr}</td>
+                      </tr>
+                    </>
+                  );
+                })}
               </tbody>
             </table>
           </Deferred>
