6de21d5b0198d77f7e7316632e112608722bb634
FallBackITA27 <diegomassanova4@gmail.com>
2024-11-10 22:27:56 +0100

    revert useApi hook changes

--- src/hooks/ApiHook.ts
+++ src/hooks/ApiHook.ts
@@ -5,24 +5,20 @@ export interface ApiState<T> {
   isLoading: boolean;
   data?: T;
   error?: ResponseError;
-  timestamp: Date;
 }
 
-export const useApi = <T>(
-  apiCallback: () => Promise<T>,
-  dependencies: DependencyList = [],
-  timestamp: Date = new Date(),
-) => {
-  const initialState = { isLoading: true, timestamp };
+export const useApi = <T>(apiCallback: () => Promise<T>, dependencies: DependencyList = []) => {
+  const initialState = { isLoading: true };
   const [state, setState] = useState<ApiState<T>>(initialState);
 
   useEffect(() => {
+    setState((prev) => ({ ...prev, isLoading: true }));
     apiCallback()
       .then((data: T) => {
-        setState({ isLoading: false, data, timestamp });
+        setState({ isLoading: false, data });
       })
       .catch((error: ResponseError) => {
-        setState({ isLoading: false, error, timestamp });
+        setState({ isLoading: false, error });
       });
     // React complains about apiCallback missing from dependency array, but it isn't needed.
     // In fact, it causes the effect hook to be triggered in an endless loop. We don't want that.
@@ -37,11 +33,9 @@ export const useApiArray = <T, R>(
   callCount: number,
   paramArray: R[],
   dependencies: DependencyList = [],
-  timestamp: Date = new Date(),
 ) => {
   const initialState = Array.from({ length: callCount }, (_, __) => ({
     isLoading: true,
-    timestamp,
   }));
   const [state, setState] = useState<Array<ApiState<T>>>(initialState);
 
@@ -61,14 +55,14 @@ export const useApiArray = <T, R>(
         .then((data: T) => {
           setState((prev) => [
             ...prev.slice(0, i),
-            { isLoading: false, data, timestamp },
+            { isLoading: false, data },
             ...prev.slice(i + 1),
           ]);
         })
         .catch((error: ResponseError) => {
           setState((prev) => [
             ...prev.slice(0, i),
-            { isLoading: false, error, timestamp },
+            { isLoading: false, error },
             ...prev.slice(i + 1),
           ]);
         });
