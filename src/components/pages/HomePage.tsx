3778ab61d23f3c68e23df6e6b5bb6b1ac6a4518f
FallBackITA27 <diegomassanova4@gmail.com>
2024-11-17 21:16:09 +0100

    home page text test

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,8 +3,13 @@ import { coreApi } from "../../api";
 import { useApi } from "../../hooks";
 import { BlogPostModule } from "../widgets";
 import DiscordEmbed from "../widgets/DiscordEmbed";
+import ExpandableModule from "../widgets/ExpandableModule";
+import { I18nContext } from "../../utils/i18n/i18n";
+import { useContext } from "react";
 
 const HomePage = () => {
+    const { translations, lang } = useContext(I18nContext);
+    
   const { isLoading: blogpostsLoading, data: posts } = useApi(
     () => coreApi.coreBlogLatestList(),
     [],
@@ -12,12 +17,15 @@ const HomePage = () => {
   );
 
   return (
-    <>
+    <div className="homePageGrid">
+      <ExpandableModule moduleHeight={150} heading={translations.homePageWelcomeHeading[lang]}>
+        <div className="module-content">{translations.homePageWelcomeParagraph[lang]}</div>
+      </ExpandableModule>
       <DiscordEmbed />
       <Deferred isWaiting={blogpostsLoading}>
         {posts?.slice(0, 4)?.map((post) => <BlogPostModule post={post} />)}
       </Deferred>
-    </>
+    </div>
   );
 };
 
