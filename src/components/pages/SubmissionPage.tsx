f7562a76bd56b02986076785f795bfbeaaa66c39
FallBackITA27 <diegomassanova4@gmail.com>
2025-01-04 00:33:18 +0100

    Add Submission Filter by Submitter/Player

--- src/components/pages/SubmissionPage.tsx
+++ src/components/pages/SubmissionPage.tsx
@@ -11,14 +11,23 @@ import { useApi } from "../../hooks";
 import { I18nContext, translate } from "../../utils/i18n/i18n";
 import SubmissionForm from "../widgets/SubmissionForm";
 import SubmissionCard from "../widgets/SubmissionCard";
+import RadioButtons from "../widgets/RadioButtons";
 
 const SubmitTab = () => {
   return <SubmissionForm />;
 };
 
+enum SubmissionFilter {
+  All = 1,
+  ByYou = 2,
+  ForYou = 3,
+}
+
 const SubmissionsTab = () => {
   const metadata = useContext(MetadataContext);
+  const { user } = useContext(UserContext);
   const [reload, setReload] = useState(Math.random());
+  const [filter, setFilter] = useState<SubmissionFilter>(SubmissionFilter.All);
 
   const { isLoading, data: submissions } = useApi(
     () => api.timetrialsSubmissionsList(),
@@ -28,11 +37,26 @@ const SubmissionsTab = () => {
 
   return (
     <div className="module-content">
+      <RadioButtons
+        state={filter}
+        setState={setFilter}
+        data={[
+          { text: "Submitted By You", value: SubmissionFilter.ByYou },
+          { text: "Submitted For You", value: SubmissionFilter.ForYou },
+          { text: "All your Submissions", value: SubmissionFilter.All },
+        ]}
+      />
       <Deferred isWaiting={isLoading || metadata.isLoading}>
         <div key={reload} className="card-container">
-          {submissions?.map((submission) => (
-            <SubmissionCard setReload={setReload} submission={submission} />
-          ))}
+          {submissions
+            ?.filter((submission) =>
+              SubmissionFilter.All
+                ? true
+                : SubmissionFilter.ByYou
+                  ? submission.submittedBy.player.id === user?.player
+                  : submission.player.id === user?.player,
+            )
+            .map((submission) => <SubmissionCard setReload={setReload} submission={submission} />)}
         </div>
       </Deferred>
     </div>
