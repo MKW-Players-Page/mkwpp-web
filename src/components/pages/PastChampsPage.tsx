7050ecc81ccffc71f19bb1a8c2db6a2520ef0803
FallBackITA27 <diegomassanova4@gmail.com>
2025-01-11 00:13:18 +0100

    Past Champs UI

--- src/components/pages/PastChampsPage.tsx
+++ src/components/pages/PastChampsPage.tsx
@@ -10,6 +10,7 @@ import OverwriteColor from "../widgets/OverwriteColor";
 import { SettingsContext } from "../../utils/Settings";
 import PlayerMention from "../widgets/PlayerMention";
 import { CategoryRadio } from "../widgets/CategorySelect";
+import ArrayTable from "../widgets/Table";
 
 const PastChampsPage = () => {
   const { lang } = useContext(I18nContext);
@@ -35,53 +36,62 @@ const PastChampsPage = () => {
         <CategoryRadio value={category} onChange={setCategory} />
         <Deferred isWaiting={isLoading}>
           <div className="module table-hover-rows">
-            <table>
-              <thead>
-                <th>{translate("pastChampsPagePlayerCol", lang)}</th>
-                <th>{translate("pastChampsPageStartDateCol", lang)}</th>
-                <th>{translate("pastChampsPageEndDateCol", lang)}</th>
-                <th>{translate("pastChampsPageDurationCol", lang)}</th>
-                <th>{translate("pastChampsPagePercentCol", lang)}</th>
-              </thead>
-              <tbody>
-                {champs?.map((champ, idx, arr) => {
+            <ArrayTable
+              headerRows={[
+                [
+                  { content: translate("pastChampsPagePlayerCol", lang) },
+                  { content: translate("pastChampsPageStartDateCol", lang) },
+                  {
+                    content: translate("pastChampsPageEndDateCol", lang),
+                    className: "past-champs-s1",
+                  },
+                  { content: translate("pastChampsPageDurationCol", lang) },
+                  { content: translate("pastChampsPagePercentCol", lang) },
+                ],
+              ]}
+              rows={
+                champs?.map((champ, idx, arr) => {
                   const nextExists = arr[idx + 1] !== undefined;
                   const duration = nextExists
                     ? arr[idx + 1].dateInstated - champ.dateInstated
                     : Math.floor(+new Date() / 1000) - champ.dateInstated;
                   const durationDays = duration / 86400;
                   const durationPerc = (duration / totalDuration) * 100;
-                  return (
-                    <tr>
-                      <td>
+                  return [
+                    {
+                      content: (
                         <PlayerMention
                           precalcPlayer={champ.player}
                           precalcRegionId={champ.player.region ?? undefined}
                           xxFlag={true}
                         />
-                      </td>
-                      <td>{new Date(champ.dateInstated * 1000).toLocaleDateString(lang)}</td>
-                      <td>
-                        {nextExists
-                          ? new Date(arr[idx + 1].dateInstated * 1000).toLocaleDateString(lang)
-                          : translate("pastChampsPageOngoing", lang)}
-                      </td>
-                      <td>
-                        {durationDays < 1
+                      ),
+                    },
+                    {
+                      content: new Date(champ.dateInstated * 1000).toLocaleDateString(lang),
+                    },
+                    {
+                      content: nextExists
+                        ? new Date(arr[idx + 1].dateInstated * 1000).toLocaleDateString(lang)
+                        : translate("pastChampsPageOngoing", lang),
+                      className: "past-champs-s1",
+                    },
+                    {
+                      content:
+                        durationDays < 1
                           ? translate("pastChampsPageLessThan1Day", lang)
-                          : `${Math.floor(durationDays)} ${Math.floor(durationDays) === 1 ? translate("pastChampsPageDaySingular", lang) : translate("pastChampsPageDayPlural", lang)}`}
-                      </td>
-                      <td>
-                        {durationPerc < 10
+                          : `${Math.floor(durationDays)} ${Math.floor(durationDays) === 1 ? translate("pastChampsPageDaySingular", lang) : translate("pastChampsPageDayPlural", lang)}`,
+                    },
+                    {
+                      content:
+                        (durationPerc < 10
                           ? "0" + durationPerc.toFixed(5)
-                          : durationPerc.toFixed(5)}
-                        %
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                          : durationPerc.toFixed(5)) + "%",
+                    },
+                  ];
+                }) ?? [[]]
+              }
+            />
           </div>
         </Deferred>
       </OverwriteColor>
