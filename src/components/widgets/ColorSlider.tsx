5e085b5722b8510eed9efeec91d1e23e04ce2b57
FallBackITA27 <diegomassanova4@gmail.com>
2025-01-11 00:31:28 +0100

    Options Page UI (Color Slider)

--- src/components/widgets/ColorSlider.tsx
+++ src/components/widgets/ColorSlider.tsx
@@ -17,17 +17,12 @@ const ColorSlider = ({ paragraphText, valueKey }: ColorSliderProps) => {
   const { lang } = useContext(I18nContext);
   const { settings, setSettings } = useContext(SettingsContext);
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: "5px",
-        alignItems: "center",
-        width: "min(450px,35%)",
-        justifyContent: "space-between",
-      }}
-    >
-      <p style={{ width: "120px", whiteSpace: "nowrap" }}>{paragraphText}</p>
+    <div className="color-slider">
+      <div style={{ flexGrow: 1, width: "100px", maxWidth: "170px", whiteSpace: "nowrap" }}>
+        {paragraphText}
+      </div>
       <input
+        style={{ flexGrow: 1, maxWidth: "300px" }}
         className="rainbow-slider"
         type="range"
         defaultValue={(settings as any)[valueKey]}
@@ -36,6 +31,7 @@ const ColorSlider = ({ paragraphText, valueKey }: ColorSliderProps) => {
         onChange={(e) => setSettings(setSettingKV(settings, valueKey, parseInt(e.target.value)))}
       />
       <button
+        style={{ flexShrink: 10, marginBottom: "16px" }}
         onClick={(e) => {
           setSettings(setSettingKV(settings, valueKey, browserSettingDefault(valueKey) as number));
           (e.target as any).previousSibling.value = browserSettingDefault(valueKey);
