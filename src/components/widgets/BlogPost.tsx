46ad9efeaab1428d0cedee91ab4f0e5c360cca60
FallBackITA27 <diegomassanova4@gmail.com>
2025-01-22 21:18:03 +0100

    HomePage BlogPost Title redirect to Archive

--- src/components/widgets/BlogPost.tsx
+++ src/components/widgets/BlogPost.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { BlogPost } from "../../api";
 import { I18nContext } from "../../utils/i18n/i18n";
+import { Pages, resolvePage } from "../pages";
 
 export interface BlogPostModuleProps {
   post: BlogPost;
@@ -12,7 +14,13 @@ const BlogPostModule = ({ post, style }: BlogPostModuleProps) => {
   return (
     <div style={style} className="module">
       <div className="module-header">
-        <b>{post.title}</b>
+        {window.location.pathname === "/mkw/" ? (
+          <Link to={resolvePage(Pages.BlogPost, { id: post.id })}>
+            <b>{post.title}</b>
+          </Link>
+        ) : (
+          <b>{post.title}</b>
+        )}
         <br />
         {new Date(post.publishedAt * 1000)
           .toLocaleString(lang)
