df85846791f739fd7eb8f913a6c300b6d7897cd6
FallBackITA27 <diegomassanova4@gmail.com>
2025-01-27 20:55:54 +0100

    Create Pagination Buttons

--- src/components/pages/RankingsPage.tsx
+++ src/components/pages/RankingsPage.tsx
@@ -13,6 +13,7 @@ import RegionSelectionDropdown from "../widgets/RegionDropdown";
 import {
   useCategoryParam,
   useLapModeParam,
+  usePageNumber,
   useRegionParam,
   useRowHighlightParam,
 } from "../../utils/SearchParams";
@@ -22,6 +23,7 @@ import PlayerMention from "../widgets/PlayerMention";
 import { CategoryRadio } from "../widgets/CategorySelect";
 import { LapModeRadio } from "../widgets/LapModeSelect";
 import ArrayTable, { ArrayTableCellData, ArrayTableData } from "../widgets/Table";
+import { PaginationButtonRow } from "../widgets/PaginationButtons";
 
 export interface RankingsMetric {
   titleKey: TranslationKey;
@@ -92,6 +94,7 @@ const RankingsPage = ({ metric }: RankingsProps) => {
   const { category, setCategory } = useCategoryParam(searchParams, ["hl"]);
   const { lapMode, setLapMode } = useLapModeParam(searchParams, false, ["hl"]);
   const { region, setRegion } = useRegionParam(searchParams);
+  const { pageNumber, setPageNumber } = usePageNumber(searchParams);
   const highlight = useRowHighlightParam(searchParams).highlight;
 
   const { lang } = useContext(I18nContext);
@@ -197,6 +200,11 @@ const RankingsPage = ({ metric }: RankingsProps) => {
             setValue={setRegion}
           />
         </div>
+        <PaginationButtonRow
+          selectedPage={pageNumber}
+          setSelectedPage={setPageNumber}
+          numberOfPages={15}
+        />
         <div className="module table-hover-rows">
           <Deferred isWaiting={isLoading}>
             <ArrayTable
