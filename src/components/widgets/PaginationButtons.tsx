aac65207cd5c1ca0b2d019b312bcc52d9195a797
FallBackITA27 <diegomassanova4@gmail.com>
2025-01-27 21:18:10 +0100

    deal with edge cases

--- src/components/widgets/PaginationButtons.tsx
+++ src/components/widgets/PaginationButtons.tsx
@@ -94,10 +94,19 @@ export const PaginationButtonRow = ({
   setSelectedPage,
   numberOfPages,
 }: PaginationProps) => {
+  if (numberOfPages !== 0) {
+    if (selectedPage > numberOfPages) {
+      setSelectedPage(numberOfPages);
+    } else if (selectedPage < 1) setSelectedPage(1);
+  }
+
   return (
-    <div style={{ justifyContent: "center" }} className="module-row wrap">
+    <div
+      style={{ justifyContent: "center", display: numberOfPages > 1 ? "" : "none" }}
+      className="module-row wrap"
+    >
       <div className="module pagination-button-row">
-        {selectedPage !== 1 && (
+        {selectedPage !== 1 && numberOfPages > 3 && (
           <PreviousButton
             pageNumber={Math.max(1, selectedPage - 1)}
             setSelectedPage={setSelectedPage}
@@ -108,29 +117,33 @@ export const PaginationButtonRow = ({
           selected={selectedPage === 1}
           setSelectedPage={setSelectedPage}
         />
-        <ButtonNumber
-          pageNumber={
-            selectedPage === 1
-              ? 2
-              : selectedPage === numberOfPages
-                ? numberOfPages - 1
-                : selectedPage
-          }
-          selected={!(selectedPage === 1 || selectedPage === numberOfPages)}
-          setSelectedPage={setSelectedPage}
-        />
+        {numberOfPages !== 2 && (
+          <ButtonNumber
+            pageNumber={
+              selectedPage === 1
+                ? 2
+                : selectedPage === numberOfPages
+                  ? numberOfPages - 1
+                  : selectedPage
+            }
+            selected={!(selectedPage === 1 || selectedPage === numberOfPages)}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
         <ButtonNumber
           pageNumber={numberOfPages}
           selected={selectedPage === numberOfPages}
           setSelectedPage={setSelectedPage}
         />
-        {selectedPage !== numberOfPages && (
+        {selectedPage !== numberOfPages && numberOfPages > 3 && (
           <NextButton
             pageNumber={Math.min(numberOfPages, selectedPage + 1)}
             setSelectedPage={setSelectedPage}
           />
         )}
-        <ButtonNumberTypeable maxPageNumber={numberOfPages} setSelectedPage={setSelectedPage} />
+        {numberOfPages > 3 && (
+          <ButtonNumberTypeable maxPageNumber={numberOfPages} setSelectedPage={setSelectedPage} />
+        )}
       </div>
     </div>
   );
