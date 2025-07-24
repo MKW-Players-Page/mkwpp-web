53fc29ab5d5e9315ceaa7ab5d83ba2a999597e98
falb <diegomassanova4@gmail.com>
2025-07-24 18:22:20 +0200

    add button to display even older times

--- src/components/pages/TrackListPage.tsx
+++ src/components/pages/TrackListPage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { I18nContext, translate } from "../../utils/i18n/i18n";
 import CupsList from "../widgets/CupsList";
@@ -7,12 +7,31 @@ import RecentTimes from "../widgets/RecentTimes";
 const TrackListPage = () => {
   const { lang } = useContext(I18nContext);
 
+  const [limit, setLimit] = useState(30);
+
   return (
     <>
       <h1>{translate("trackListPageHeading", lang)}</h1>
       <CupsList />
-      <RecentTimes records limit={30} />
-      <RecentTimes records={false} limit={30} />
+      <p>{translate("trackListPageMoreTimes", lang)}</p>
+      <input
+        style={{ marginBottom: "16px" }}
+        type="number"
+        defaultValue={30}
+        onChange={(e) => {
+          let value = parseInt(e.target.value);
+          if (isNaN(value)) {
+            e.target.value = String((value = 30));
+          } else if (value < 1) {
+            e.target.value = String((value = 1));
+          }
+          setLimit(value);
+        }}
+        min="1"
+        pattern="[0-9]*"
+      />
+      <RecentTimes records limit={limit} />
+      <RecentTimes records={false} limit={limit} />
     </>
   );
 };
--- src/components/widgets/RecentTimes.tsx
+++ src/components/widgets/RecentTimes.tsx
@@ -12,7 +12,7 @@ import PlayerMention from "./PlayerMention";
 
 import "./RecentTimes.css";
 import { SmallBigDateFormat, SmallBigTrackFormat } from "./SmallBigFormat";
-import ArrayTable from "./Table";
+import ArrayTable, { ArrayTableCellData } from "./Table";
 import { secondsToDate } from "../../utils/DateUtils";
 
 interface RecentTimesProps {
@@ -20,13 +20,75 @@ interface RecentTimesProps {
   limit: number;
 }
 
-const RecentTimes = ({ records, limit }: RecentTimesProps) => {
+const RecentTimes = ({ records = false, limit }: RecentTimesProps) => {
   const { lang } = useContext(I18nContext);
   const metadata = useContext(MetadataContext);
 
-  const { isLoading: recentTimesLoading, data: recentTimes } = useApi(
-    () => Score.getRecent(limit, !!records),
-    [metadata.isLoading],
+  const { isLoading: recentTimesLoading, data } = useApi(
+    () =>
+      Score.getRecent(limit, records).then((scores) =>
+        scores.reduce(
+          (acc, data, index) => {
+            const track = metadata.getTrackById(data.trackId);
+            acc.rows.push([
+              {
+                content: (
+                  <Link
+                    to={resolvePage(
+                      Pages.TrackChart,
+                      { id: data.trackId },
+                      {
+                        cat: data.category !== CategoryEnum.NonShortcut ? data.category : null,
+                        lap: data.isLap ? LapModeEnum.Lap : null,
+                        hl: data.value,
+                      },
+                    )}
+                  >
+                    <SmallBigTrackFormat track={track} bigClass="b3" smallClass="s3" />
+                  </Link>
+                ),
+                lockedCell: true,
+              },
+              {
+                content: translateCategoryName(data.category, lang),
+                className: "b2",
+              },
+              {
+                content: data.isLap
+                  ? translate("constantLapModeLap", lang)
+                  : translate("constantLapModeCourse", lang),
+                className: "b1",
+              },
+              {
+                content: (
+                  <PlayerMention
+                    playerOrId={data.player}
+                    regionOrId={data.player.regionId ?? undefined}
+                    xxFlag
+                  />
+                ),
+              },
+              {
+                content: formatTime(data.value),
+              },
+              {
+                content: (
+                  <SmallBigDateFormat
+                    date={secondsToDate(data.date)}
+                    smallClass={"s1 b4"}
+                    bigClass={"b1"}
+                  />
+                ),
+                className: "",
+              },
+            ]);
+            acc.rowKeys.push(String(index));
+            return acc;
+          },
+          { rows: [] as ArrayTableCellData[][], rowKeys: [] as string[] },
+        ),
+      ),
+    [metadata.isLoading, limit],
     records ? "recentRecords" : "recentTimes",
   );
 
@@ -61,63 +123,8 @@ const RecentTimes = ({ records, limit }: RecentTimesProps) => {
               { content: translate("recentTimesRecentTimesDateCol", lang) },
             ],
           ]}
-          rows={
-            recentTimes?.map((data) => {
-              const track = metadata.getTrackById(data.trackId);
-              return [
-                {
-                  content: (
-                    <Link
-                      to={resolvePage(
-                        Pages.TrackChart,
-                        { id: data.trackId },
-                        {
-                          cat: data.category !== CategoryEnum.NonShortcut ? data.category : null,
-                          lap: data.isLap ? LapModeEnum.Lap : null,
-                          hl: data.value,
-                        },
-                      )}
-                    >
-                      <SmallBigTrackFormat track={track} bigClass="b3" smallClass="s3" />
-                    </Link>
-                  ),
-                  lockedCell: true,
-                },
-                {
-                  content: translateCategoryName(data.category, lang),
-                  className: "b2",
-                },
-                {
-                  content: data.isLap
-                    ? translate("constantLapModeLap", lang)
-                    : translate("constantLapModeCourse", lang),
-                  className: "b1",
-                },
-                {
-                  content: (
-                    <PlayerMention
-                      playerOrId={data.player}
-                      regionOrId={data.player.regionId ?? undefined}
-                      xxFlag
-                    />
-                  ),
-                },
-                {
-                  content: formatTime(data.value),
-                },
-                {
-                  content: (
-                    <SmallBigDateFormat
-                      date={secondsToDate(data.date)}
-                      smallClass={"s1 b4"}
-                      bigClass={"b1"}
-                    />
-                  ),
-                  className: "",
-                },
-              ];
-            }) ?? [[]]
-          }
+          tableData={{ rowKeys: data?.rowKeys ?? [] }}
+          rows={data?.rows ?? [[]]}
         />
       </Deferred>
     </ExpandableModule>
