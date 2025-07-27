4e209578e19bead6f22216e883e9386efcb300d0
Leila Massanova <49619073+FallBackITA27@users.noreply.github.com>
2025-07-27 22:10:20 +0200

    minor changes (#107)
    * update flag imports script
    
    * update i18n.json
    
    * add new backend endpoint
    
    * optimize icons via SVGO
    
    * move SVGO to dev libraries

--- codegen-scripts/regen-flag-imports.js
+++ codegen-scripts/regen-flag-imports.js
@@ -8,28 +8,20 @@ const fs = require("fs");
     key: filename
     value: region code
 */
-const renameScheme = {
-  un: "world",
-  eu: "eur",
-  pc: "oce",
-};
+
 const flagFileNames = fs.readdirSync("src/assets/flags");
 
 let out1 = ""; // imports
 let out2 = ""; // Flags const
-let out3 = ""; // map function
 
 for (const flagFileName of flagFileNames) {
   const fileNameNoExt = flagFileName.split(".")[0];
-  const renamed = renameScheme[fileNameNoExt] !== undefined;
-  const code = renamed ? renameScheme[fileNameNoExt] : fileNameNoExt;
-  out1 += `import { ReactComponent as Flag${code.toUpperCase()} } from "../../assets/flags/${flagFileName}";${renamed ? " // TODO: RENAMED!" : ""}\n`;
-  out2 += `  ${code}: Flag${code.toUpperCase()},\n`;
-  if (renamed) out3 += `    case "${code}":\n      return "${fileNameNoExt}";\n`;
+  out1 += `import { ReactComponent as Flag${fileNameNoExt.toUpperCase()} } from "../../assets/flags/${flagFileName}";\n`;
+  out2 += `  ${fileNameNoExt}: Flag${fileNameNoExt.toUpperCase()},\n`;
 }
 
 if (!fs.existsSync("temp/")) fs.mkdirSync("temp");
 fs.writeFileSync(
   "temp/flag-imports.txt",
-  `import "./Flags.css";\n\n${out1}\nexport const Flags = {\n${out2}};\n\n/**\n * @param flag flag code that comes from the code field in region data\n * @returns the code used for the svg file instead\n */\nconst codeToFlag = (flag: string): string => {\n  switch (flag) {\n${out3}    default:\n      return flag;\n  }\n};`,
+  `import "./Flags.css";\n\n${out1}\nexport const Flags = {\n${out2}};\n`,
 );
